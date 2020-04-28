1e4e0f2b359757c1d952bf0c258fd9dc2bea7500
marcelo-sl <mar.sousa2061@gmail.com>
2020-04-28 18:17:15 -0300

    FRONTEND: Adding validation on Login and Register pages

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 import Header from '../../components/Header';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 export default function Login() {
+    const validations = yup.object().shape({
+        email: yup.string().email().required(),
+        password: yup.string().min(8).required(),
+    });
+
     const history = useHistory();
 
-    const handleLoginForm = data => {
+    const handleLoginForm = values => {
+        api.post('authenticate', values).then(res => {
+            const { token } = res.data;
+            localStorage.setItem('user-token', token);
+        });
+
         history.push('/');
     }
 
     return (
         <>
             <Header />
-            <form className="login-form" onSubmit={handleLoginForm}>
-                <h1>Fazer Login</h1>
-                <input type="email" name="email" placeholder="E-mail"/><br/>
-                <input type="password" name="password" placeholder="Senha"/><br/>
-                
-                <div className="register-link"><span>Quero <Link to="/register">criar</Link> uma conta!</span></div>
+            <Formik initialValues={{}} onSubmit={handleLoginForm} validationSchema={validations}>
+                <Form className="login-form" >
+                    <h1>Fazer Login</h1>
+                    <div className="form-group">
+                        <Field name="email" placeholder="E-mail"/>
+                        <ErrorMessage component="div" name="email" className="error-msg"/>
+                    </div>
+                    <div className="form-group">
+                        <Field type="password" name="password" placeholder="Senha"/>
+                        <ErrorMessage component="div" name="password" className="error-msg"/>
+                    </div>
+                    
+                    <div className="register-link"><span>Quero <Link to="/register">criar</Link> uma conta!</span></div>
 
-                <button type="submit">Login</button>
-            </form>
+                    <button type="submit">Login</button>
+                </Form>
+            </Formik>
         </>
     )
 }
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,31 +1,54 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 import Header from '../../components/Header';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 export default function Register() {
+    const validations = yup.object().shape({
+        email: yup.string().email().required(),
+        name: yup.string().required(),
+        password: yup.string().min(8).required(),
+    });
+
     const history = useHistory();
 
-    const handleRegisterForm = data => {
-        history.push('/');
+    const handleRegisterForm = values => {
+        api.post('register', values).then(() => {
+            history.push('/login');
+            alert('Usuário criado!');
+        });
     }
     
     return (
         <>
             <Header />
-            <form className="register-form" onSubmit={handleRegisterForm}>
-                <h1>Criar Conta</h1>
-                <input type="email" name="email" placeholder="E-mail"/><br/>
-                <input type="text" name="name" placeholder="Nome"/><br/>
-                <input type="password" name="password" placeholder="Senha"/><br/>
-                <input type="password" name="confirm-password" placeholder="Confirmar senha"/><br/>
-                
-                <div className="login-link"><span>Já tenho uma conta, fazer <Link to="/login">login</Link> com minha conta!</span></div>
+            <Formik initialValues={{}} onSubmit={handleRegisterForm} validationSchema={validations}>
+                <Form className="register-form">
+                    <h1>Criar Conta</h1>
+                    <div className="form-group">
+                        <Field name="email" placeholder="E-mail"/> 
+                        <ErrorMessage component="div" name="email" className="error-msg"/> 
+                    </div>
+                    <div className="form-group">
+                        <Field name="name" placeholder="Nome"/> 
+                        <ErrorMessage component="div" name="name" className="error-msg"/> 
+                    </div>
+                    <div className="form-group">
+                        <Field type="password" name="password" placeholder="Senha"/> 
+                        <ErrorMessage component="div" name="password" className="error-msg"/> 
+                    </div>
+                    
+                    <div className="login-link"><span>Já tenho uma conta, fazer <Link to="/login">login</Link> com minha conta!</span></div>
 
-                <button type="submit">Cadastrar</button>
-            </form>
+                    <button type="submit">Cadastrar</button>
+                </Form>
+            </Formik>
         </>
     )
 }
